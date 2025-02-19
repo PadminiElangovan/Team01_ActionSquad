891b9f41fb25db457a21fdc401120bc2ddc78b23
Padmini <PadminiElangovan>
2025-02-19 00:59:26 -0600

    Added Home FeatureFile

---
+++ src/test/java/stepDefinitions/DashboardStep.java
@@ -0,0 +1,101 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DashboardStep {
+	
+
+@Given("Admin is on login Page")
+public void admin_is_on_login_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("Admin enter valid data in all field and clicks login button")
+public void admin_enter_valid_data_in_all_field_and_clicks_login_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see LMS -Learning management system as title")
+public void admin_should_see_lms_learning_management_system_as_title() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("LMS title should be on the top left corner of page")
+public void lms_title_should_be_on_the_top_left_corner_of_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see correct spelling in navigation bar text")
+public void admin_should_see_correct_spelling_in_navigation_bar_text() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see correct spelling and space in LMS title")
+public void admin_should_see_correct_spelling_and_space_in_lms_title() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see the navigation bar text on the top right side")
+public void admin_should_see_the_navigation_bar_text_on_the_top_right_side() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see {string} in the {string}")
+public void admin_should_see_in_the(String Menu, String Order) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see piechart")
+public void admin_should_see_piechart() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see welcome message with user name and role")
+public void admin_should_see_welcome_message_with_user_name_and_role() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see bar chart for Active and inactive user")
+public void admin_should_see_bar_chart_for_active_and_inactive_user() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see {string} count")
+public void admin_should_see_count(String Section) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see staff table with pagination icons")
+public void admin_should_see_staff_table_with_pagination_icons() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see {int} staff data in a page")
+public void admin_should_see_staff_data_in_a_page(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Admin should see {string} page icon disabled")
+public void admin_should_see_page_icon_disabled(String PageArrow) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+}
