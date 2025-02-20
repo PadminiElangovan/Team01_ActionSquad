f4f43b308b904e3116f05fefc24b6272489efbfa
nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
2025-02-20 13:22:25 -0700

    Program validation

--- src/test/java/appHook/TestContext.java
+++ src/test/java/appHook/TestContext.java
@@ -1,12 +1,16 @@
-package appHook;
+	package appHook;
+
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import common.ConfigReader;
-import driverFactory.DriverConfig;
 
-import java.time.Duration;
+import common.ConfigReader;
+import common.ExcelReader;
+import driverFactory.DriverConfig;
+import pages.Dashboard_Page;
+import pages.Login_Page;
 
 public class TestContext {
     
@@ -15,6 +19,9 @@ public class TestContext {
 	private WebDriverWait wait;
 	private ConfigReader configReader;
 	private Actions actions;
+	private ExcelReader excelReader;
+	private Login_Page Login;
+	private Dashboard_Page Dashboard;
 	
 	
 	public TestContext() {
@@ -26,33 +33,47 @@ public class TestContext {
 	public void setDriver(WebDriver driver) {
 		this.driver = driver;
 		this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		this.actions = new Actions(driver); // Initialize Actions here, after driver is set
-			}
+		this.actions = new Actions(driver);
+		this.excelReader= new ExcelReader();
+		this.Login = new Login_Page(driver, this);
+		this.Dashboard = new Dashboard_Page(driver, this);
+		}
 
 	public DriverConfig getDriverConfig() {
 		return driverConfig;
-	}
+		}
 
 	// WebDriver instance
 	public WebDriver getDriver() {
-		return driver;
-	}
-
-   // driver close
+		return driver; 
+		}
+	
 	public void quitDriver() {
 		driverConfig.quitDriver();
-	}
+		}
 
 	public WebDriverWait getWait() {
 		return wait;
-	}
+		}
 
 	public String getApplicationURL() {
 		return configReader.getApplicationURL(); // Method to get URL
-	}
-
-		public Actions getActions() {
+		}
+	
+	public Actions getActions() {
 		return actions;
+		}
+	
+	public Login_Page getLogin() {
+		return Login;
+	}
+	
+	public Dashboard_Page getDashboard() {
+			return Dashboard;
+		} 
+
+	public ExcelReader getExcelReader() {
+	    return excelReader;
 	}
 
 	
--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -8,12 +8,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", //Allure Report
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", //Extent Report
-				"json:target/CucumberReports/DSAlgoJson.json","html:target/CucumberReports/DSAlgoHtml.html", //Cucumber Report
+				"json:target/CucumberReports/DSAlgoJson.json","html:target/CucumberReports/LMS.html", //Cucumber Report
 				"com.aventstack.chaintest.plugins.ChainTestCucumberListener:", //ChainTest Report
 				"rerun:target/rerun.txt"}, //Rerun failed Scenario
 		monochrome=false,  //console output color
 		tags="@LMS", //tags from feature file
-		features = {"src/test/resources/Feature files"}, //location of feature files
+		features = {"src/test/resources/Feature files/03_Program.feature"}, //location of feature files
 		glue= {"stepDefinitions","appHook"}) //location of step definition files
 
 	public class TestRunner extends AbstractTestNGCucumberTests{
