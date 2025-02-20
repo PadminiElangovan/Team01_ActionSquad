f752e4619645c03143899f005b8a41da00b047b3
nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
2025-02-20 13:19:56 -0700

    Program Validation

--- src/test/java/pages/Program_Page.java
+++ src/test/java/pages/Program_Page.java
@@ -1,5 +1,147 @@
 package pages;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import appHook.TestContext;
+
 public class Program_Page {
+
+	private WebDriver driver;
+	private WebDriverWait wait;
+	Actions action;
+	TestContext context;
+
+	@FindBy(xpath = "//*[@id='program']")
+	private WebElement programBtn;
+	@FindBy(xpath = "//*[@id='mat-menu-panel-0']/div/button")
+	private WebElement addNewPgm;
+	@FindBy(xpath = "//*[@id='filterGlobal']")
+	private WebElement searchbtn;
+	@FindBy(xpath = "//span[text()=' LMS - Learning Management System ']")
+	private WebElement appHeader;
+	@FindBy(xpath = "//div[@class='ng-star-inserted']")
+	private WebElement menu;
+	@FindBy(xpath = "//span[text()='Home']")
+	private WebElement homeHeader;
+	@FindBy(xpath = "//span[text()='Batch']")
+	private WebElement batchHeader;
+	@FindBy(xpath = "//span[text()='Class']")
+	private WebElement classHeader;
+	@FindBy(xpath = "//span[text()='Logout']")
+	private WebElement logoutHeader;
+
+	@FindBy(xpath = "//div[normalize-space()='Manage Program']")
+	private WebElement manageProgramHeader;
+	@FindBy(xpath = "//th[normalize-space()='Program Name']")
+	private WebElement programNameHeader;
+	@FindBy(xpath = "//th[normalize-space()='Program Description']")
+	private WebElement programDescriptionHeader;
+	@FindBy(xpath = "//th[normalize-space()='Program Status']")
+	private WebElement programStatus;
+	@FindBy(xpath = "//th[text()=' Edit / Delete ']")
+	private WebElement editDeleteHeader;
+	@FindBy(css = ".p-paginator-current.ng-star-inserted")
+	private WebElement showingEnteries;
+	@FindBy(className = "p-sortable-column-icon")
+	private List<WebElement> sortingBtn;
+	@FindBy(css = "button[class='p-button-danger p-button p-component p-button-icon-only']")
+	private WebElement deleteBtnMC;
+	@FindBy(css = ".p-datatable-footer.ng-star-inserted")
+	private WebElement footer;
+	@FindBy(xpath = "p-paginator-element")
+	private List<WebElement> pagination;
+
+	public Program_Page(WebDriver driver, TestContext context) {
+
+		this.driver = context.getDriver();
+		this.context = context;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		PageFactory.initElements(driver, this);
+		this.action = new Actions(driver);
+	}
+
+	public void clickProgram() {
+		programBtn.click();
+	}
+
+	public List<String> getTableHeaders() {
+		List<String> headers = new ArrayList<>();
+
+		headers.add(appHeader.getText().trim());
+		headers.add(homeHeader.getText().trim());
+		headers.add(programBtn.getText().trim());
+		headers.add(batchHeader.getText().trim());
+		headers.add(classHeader.getText().trim());
+		headers.add(logoutHeader.getText().trim());
+		headers.add(manageProgramHeader.getText().trim());
+		headers.add(programNameHeader.getText().trim());
+		headers.add(programDescriptionHeader.getText().trim());
+		headers.add(programStatus.getText().trim());
+		headers.add(addNewPgm.getText().trim());
+		headers.add(editDeleteHeader.getText().trim());
+
+		return headers;
+	}
+
+	public boolean validateShowingEnteries() {
+		return showingEnteries.isDisplayed();
+
+	}
+
+	public boolean validateSortingBtn() {
+		return isSortingbuttonDisplayed(sortingBtn);
+	}
+
+	public boolean testMultipleDeleteIsDisabled() {
+		boolean isEnabled = deleteBtnMC.isEnabled();
+		System.out.println("Is Delete button enabled? " + isEnabled);
+
+		return !isEnabled;
+	}
+
+	public boolean deleteBtnDisplayed() {
+		return deleteBtnMC.isDisplayed();
+	}
+
+	public boolean validateFooter() {
+
+		return footer.isDisplayed();
+
+	}
+
+	public boolean validatePagination() {
+		for (WebElement element : pagination) {
+			if (!element.isDisplayed()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean isSortingbuttonDisplayed(List<WebElement> elements) {
+		boolean flag = true;
+		if (elements.size() > 1) {
+			for (int i = 1; i < elements.size(); i++) {
+				WebElement element = elements.get(i);
+				if (!element.isDisplayed()) {
+					flag = false;
+					break;
+				}
+			}
+		} else {
+			flag = false;
+		}
+		return flag;
+	}
 
 }
--- src/test/java/stepDefinitions/ProgramStep.java
+++ src/test/java/stepDefinitions/ProgramStep.java
@@ -1,5 +1,82 @@
 package stepDefinitions;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import appHook.TestContext;
+import common.ExcelReader;
+import common.LoggerLoad;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.Program_Page;
+
 public class ProgramStep {
 
+	private WebDriverWait wait;
+	private WebDriver driver;
+	private TestContext context;
+	private Program_Page program;
+	private ExcelReader excelReader;
+	boolean status;
+
+	public ProgramStep(TestContext context) {
+		this.context = context;
+		this.driver = context.getDriver();
+		this.program = new Program_Page(driver, context);
+		this.excelReader = context.getExcelReader(); // Use TestContext's ExcelReader instance
+	}
+
+	@When("Admin clicks on Program")
+	public void admin_clicks_on_program() {
+		program.clickProgram();
+	}
+
+	@Then("Admin should see all fields and headers on Manage Program page")
+	public void then_admin_should_see_all_headers_match() {
+		List<String> expectedHeaders;
+		try {
+			expectedHeaders = excelReader.getColumnData("Program", 0);
+		} catch (Exception e) {
+			LoggerLoad.error("Error initializing Excel data: " + e.getMessage());
+			throw new RuntimeException("Error initializing Excel data: " + e.getMessage());
+		}
+		System.out.println(expectedHeaders);
+		List<String> actualHeaders = program.getTableHeaders();
+
+		System.out.println(actualHeaders);
+		Assert.assertTrue(actualHeaders.containsAll(expectedHeaders));
+
+	}
+
+	@Then("Admin should see footer and enabled pagination controls under the data table")
+	public void admin_should_see_footer_and_pagination() throws InterruptedException {
+		assertTrue(program.validateShowingEnteries());
+		assertTrue(program.validatePagination());
+		LoggerLoad.info("Footer is displayed");
+		LoggerLoad.info("Pagination is displayed");
+	}
+
+	@Then("Admin should see the Sort icon of all the field in the datatable.")
+	public void admin_should_see_sort_icons() {
+		status = program.validateSortingBtn();
+		assertTrue(status);
+		LoggerLoad.info("Sorting button are visible");
+	}
+
+	@Then("Admin should see the Delete button under the Manage program page header")
+	public void admin_should_see_delete_button() {
+		status = program.deleteBtnDisplayed();
+		assertTrue(status);
+		LoggerLoad.info("Delete button is visible");
+	}
+
+	@Then("Admin should see Total no of programs in below of the data table")
+	public void admin_should_see_total_numbers_of_programs() {
+		assertTrue(program.validateFooter());
+	}
 }
