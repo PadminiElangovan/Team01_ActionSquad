2026d8b3d15d7bc8782c7b1608404791061ed1ee
Padmini <PadminiElangovan>
2025-02-20 14:36:39 -0600

    Dashboard Addon feature file

--- src/test/java/pages/Dashboard_Page.java
+++ src/test/java/pages/Dashboard_Page.java
@@ -20,21 +20,40 @@ public class Dashboard_Page {
 	
 	@FindBy(xpath = "//span[contains(text(),'Learning')]") public WebElement HomeTitle;
 	@FindBy(xpath = "//mat-card-title/div") public WebElement DashboardSection;
+	@FindBy(xpath = "//app-admindata/div/div[1]") public WebElement ProfileImage;
 	@FindBy(xpath = "//app-admindata/div/div[2]") public WebElement WelcomeTxt;
 	@FindBy(xpath = "//app-admindata/div/div[3]") public WebElement role;
 	@FindBy(xpath = "//mat-toolbar//div") public WebElement MenuBar;
 	@FindBy(xpath = "//div[@class='ng-star-inserted']//span[normalize-space()]") public List<WebElement> Menus;
+	
 	@FindBy(xpath = "//div[@routerlink='/user']/div[1]") public WebElement UsrCount;
+	@FindBy(xpath = "//div[@class = 'widget green']/div[2]/div[2]") public WebElement UsrText;
+	@FindBy(xpath = "//div[@class = 'widget green']/div[1]") public WebElement UsrIcon;
+	@FindBy(xpath = "//mat-card-title/div[1]") public WebElement MngPg;
+	
 	@FindBy(xpath = "//div[@class='widget yellow']//div[@class='top']") public WebElement StaffCount;
+	@FindBy(xpath = "//div[@class = 'widget yellow']/div[2]/div[2]") public WebElement StaffText;
+	@FindBy(xpath = "//div[@class = 'widget yellow']/div[1]") public WebElement StaffIcon;
 	@FindBy(xpath = "//div[@routerlink='/batch']/div[1]") public WebElement BatchCount;
+	@FindBy(xpath = "//div[@class = 'widget red']/div[2]/div[2]") public WebElement BatchText;
+	@FindBy(xpath = "//div[@class = 'widget red']/div[1]") public WebElement BatchIcon;
 	@FindBy(xpath = "//div[@routerlink='/program']/div[1]") public WebElement ProgramCount;
+	@FindBy(xpath = "//div[@class = 'widget blue']/div[2]/div[2]") public WebElement PrgmText;
+	@FindBy(xpath = "//div[@class = 'widget blue']/div[1]") public WebElement PrgmIcon;
+	
 	@FindBy(xpath = "//app-staffdata/mat-table") public WebElement StaffTable;
+	@FindBy(xpath = "//app-staffdata/h3[contains(text(),'Data')]") public WebElement TableHeader;
+	@FindBy(xpath = "//app-staffdata//mat-header-cell[1]") public WebElement TableID;
+	@FindBy(xpath = "//app-staffdata//mat-header-cell[2]") public WebElement TableFname;
+	@FindBy(xpath = "//app-staffdata//mat-header-cell[3]") public WebElement TableLname;
+	@FindBy(xpath = "//app-staffdata//mat-header-cell[4]") public WebElement TablePhone;
 	@FindBy(xpath = "//app-staffdata/mat-paginator") public WebElement TablePagination;
 	@FindBy(xpath = "//mat-table/mat-row") public List<WebElement> DataRow;
 	@FindBy(xpath = "//button[@aria-label='First page']") public WebElement FirstPg;
 	@FindBy(xpath = "//button[@aria-label='Previous page']") public WebElement PreviousPg;
 	@FindBy(xpath = "//button[@aria-label='Next page']") public WebElement NextPg;
 	@FindBy(xpath = "//button[@aria-label='Last page']") public WebElement LastPg;
+	
 	@FindBy(xpath = "//button[@id ='dashboard']") public WebElement HomeMenu;
 	@FindBy(xpath = "//button[@id ='program']") public WebElement ProgramMenu;
 	@FindBy(xpath = "//span[text()='Batch']") public WebElement BatchMenu;
@@ -75,4 +94,21 @@ public class Dashboard_Page {
 	public void ProgramLink() {
 		ProgramMenu.click();
 	}
+	public String UsrText() {
+		return UsrText.getText();
+	}
+	public String StaffText() {
+		return StaffText.getText();
+	}
+	public String BatchText() {
+		return BatchText.getText();
+	}
+	public String PrgmText() {
+		return PrgmText.getText();
+	}
+	public String TableHeader() {
+		return TableHeader.getText();
+	}
+
+	
 }
--- src/test/java/stepDefinitions/DashboardStepAddon.java
+++ src/test/java/stepDefinitions/DashboardStepAddon.java
@@ -48,22 +48,61 @@ public void admin_should_see_welcome_text() {
 
 @Then("Admin should see Profile icon")
 public void admin_should_see_profile_icon() {
-
+	Assert.assertTrue(Dashboard.ProfileImage.isDisplayed());
 }
 
 @Then("Admin should see {string} text and icon")
-public void admin_should_see_text_and_icon(String string) {
-
+public void admin_should_see_text_and_icon(String section) {
+	switch (section) {
+	case "User":
+		String x = Loginxl.get(0).get("icon");
+		Assert.assertEquals(Dashboard.UsrText(),x);
+		Assert.assertTrue(Dashboard.UsrIcon.isDisplayed());
+		break;
+	case "Staff":
+		String y = Loginxl.get(1).get("icon");
+		Assert.assertEquals(Dashboard.StaffText(),y);
+		Assert.assertTrue(Dashboard.StaffIcon.isDisplayed());
+		break;
+	case "Batches":
+		String z = Loginxl.get(2).get("icon");
+		Assert.assertEquals(Dashboard.BatchText(),z);
+		Assert.assertTrue(Dashboard.BatchIcon.isDisplayed());
+		break;
+	case "Programs":
+		String a = Loginxl.get(3).get("icon");
+		Assert.assertEquals(Dashboard.PrgmText(),a);
+		Assert.assertTrue(Dashboard.PrgmIcon.isDisplayed());
+		break;
+	}
 }
 
 @Then("Admin should see Staff Data header")
 public void admin_should_see_staff_data_header() {
-
+	String Expected = Loginxl.get(0).get("table");
+	Assert.assertEquals(Dashboard.TableHeader(),Expected);
 }
 
 @Then("Admin should see {string} column header")
-public void admin_should_see_column_header(String string) {
-
+public void admin_should_see_column_header(String section) {
+	switch (section) {
+	case "UserID":
+		String x = Loginxl.get(1).get("table");
+		Assert.assertEquals(Dashboard.TableID.getText(),x);
+		break;
+	case "First Name":
+		String y = Loginxl.get(2).get("table");
+		Assert.assertEquals(Dashboard.TableFname.getText(),y);
+		break;
+	case "Last Name":
+		String z = Loginxl.get(3).get("table");
+		Assert.assertEquals(Dashboard.TableLname.getText(),z);
+		break;
+	case "Phone":
+		String a = Loginxl.get(4).get("table");
+		Assert.assertEquals(Dashboard.TablePhone.getText(),a);
+		break;
+	}
 }
 
 @Given("Admin is on home page after Login")
@@ -81,10 +120,61 @@ public void admin_should_remain_in_the_home_page() {
 	Assert.assertTrue(Dashboard.DashboardSection.isDisplayed());
 }
 
-@Then("Admin should be in {string} page")
-public void admin_should_be_in_page(String string) {
+@When("Admin clicks {string} icon")
+public void admin_clicks_icon(String module) throws InterruptedException{
+	System.out.println("#################################################################");
+	switch (module) {
+	case "User":
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		Dashboard.UsrIcon.click();
+		Thread.sleep(1000);
 
+		break;
+	case "Staff":
+		Dashboard.StaffIcon.click();
+		Thread.sleep(1000);
+		break;
+	case "Batches":
+		Dashboard.BatchIcon.click();
+		Thread.sleep(1000);
+		break;
+	case "Programs":
+		Dashboard.PrgmIcon.click();
+		Thread.sleep(1000);
+		break;
+	}
 }
 
+
+@Then("Admin should be in {string} page")
+public void admin_should_be_in_page(String module)  {
+	System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	String x = Loginxl.get(0).get("page");
+	switch (module) {
+	case "User":
+	
+		System.out.println(x+"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^6");
+		Assert.assertEquals(Dashboard.MngPg.getText(),x);
+		break;
+	case "Staff":
+	
+		String a = Loginxl.get(0).get("page");
+		System.out.println(a);
+		Assert.assertEquals(Dashboard.MngPg.getText(),a);
+		break;
+	case "Batches":
+		
+		String y = Loginxl.get(1).get("page");
+		System.out.println(y);
+		Assert.assertEquals(Dashboard.MngPg.getText(),y);
+		break;
+	case "Programs":
+		
+		String z = Loginxl.get(1).get("page");
+		System.out.println(z);
+		Assert.assertEquals(Dashboard.MngPg.getText(),z);
+		break;
+	}
+}
 
 }
